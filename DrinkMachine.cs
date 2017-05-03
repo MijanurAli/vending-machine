c5cc6ec0b0b52718927c18f3be441ca6e3eb399b
Mijanur Ali <mijanur.ali@sabre.com>
2017-05-02 21:11:39 -0500

    Added remaining resource files

---
+++ DrinkMachine.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpPractice
+{
+    class DrinkMachine
+    {
+        const int COST_OF_DRINK = 150;
+        public int runningTotal { get; set; }
+
+        public DrinkMachine()
+        {
+            runningTotal = 0;
+        }
+
+        public void depositCoin(int money)
+        {
+            //Only valid entries are 5, 10, 25, and 100
+            switch(money)
+            {
+                case (5):
+                    runningTotal += 5;
+                    break;
+                case (10):
+                    runningTotal += 10;
+                    break;
+                case (25):
+                    runningTotal += 25;
+                    break;
+                case (100):
+                    runningTotal += 100;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry!");
+                    break;
+            }
+        }
+
+        public bool checkTotal()
+        {
+            if (runningTotal >= COST_OF_DRINK)
+                return true;
+            else
+                return false;
+        }
+
+        public void displayDrinkMenu()
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("* C - Coke         *");
+            Console.WriteLine("* P - Pepsi        *");
+            Console.WriteLine("* S - Sprite       *");
+            Console.WriteLine("********************");
+            Console.WriteLine();
+            Console.WriteLine("Please make a selection: ");
+
+            makeDrinkSelection(Convert.ToChar(Console.ReadLine().ToUpper()));
+        }
+
+        private void makeDrinkSelection(char selection)
+        {
+            bool validSelection = false;
+            while (!validSelection)
+            {
+                switch (selection)
+                {
+                    case ('C'):
+                        Console.WriteLine("Thank you for choosing Coke!");
+                        returnChange();
+                        validSelection = true;
+                        break;
+                    case ('P'):
+                        Console.WriteLine("Thank you for choosing Pepsi!");
+                        returnChange();
+                        validSelection = true;
+                        break;
+                    case ('S'):
+                        Console.WriteLine("Thank you for choosing Sprite!");
+                        returnChange();
+                        validSelection = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice; please re-enter your selection: ");
+                        selection = Convert.ToChar(Console.ReadLine().ToUpper());
+                        validSelection = false;
+                        break;
+                }
+            }
+        }
+
+        private void returnChange()
+        {
+            if (runningTotal > COST_OF_DRINK)
+            {
+                Console.WriteLine("Your change is {0:C}", runningTotal - COST_OF_DRINK);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpPractice
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var drinkMachine = new DrinkMachine();
+
+            while (!drinkMachine.checkTotal())
+            {
+                Console.WriteLine("Please deposit money for a drink: (5, 10, 25, 100)");
+                drinkMachine.depositCoin(Convert.ToInt32(Console.ReadLine()));
+                drinkMachine.displayDrinkMenu();
+            }
+        }
+    }
+}
